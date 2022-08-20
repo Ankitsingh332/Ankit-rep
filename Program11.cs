745623b7f51f00ea86d0dd203e13a020d5edcb26
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-21 00:52:07 +0530

    Add files via upload

---
+++ Program11.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Exercise18
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\nInput first integer:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input second integer:");
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Check if both are positive:");
+        Console.WriteLine((x > 0 && y > 0) || (x > 0 && y < 0));
+    }
+}
