5eb0902b7fbfd0816b1e3fe0b75b3b4cb9716887
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-21 01:15:41 +0530

    Add files via upload

---
+++ Program12.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+public class project12
+{
+    public static void Main()
+    {
+            string str = "";
+            string str1 = string.Empty;
+            string str2;
+            Console.WriteLine("enter first number: ");
+            string i1 = Console.ReadLine();
+            Console.WriteLine("enter second number: ");
+            string i2 = Console.ReadLine();
+            bool b1 = string.IsNullOrEmpty(i1);
+            Console.WriteLine(b1);
+            bool b2 = string.IsNullOrEmpty(i2);
+            Console.WriteLine(b1);
+        }
+    }
+
