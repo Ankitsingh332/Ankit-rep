0c42ad50dce7f999e2f079daf8f60a0e12b445b6
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-21 01:20:39 +0530

    Add files via upload

---
+++ Program16.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+public class project16
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the first name");
+        string str = Console.ReadLine();
+        Console.WriteLine("Enter the last name");
+        string str1 = Console.ReadLine();
+        if (string.IsNullOrEmpty(str) && string.IsNullOrEmpty(str1))
+        {
+            Console.WriteLine("Enter a valid FirstName and lastName");
+        }
+        else
+            Console.WriteLine($"Welcome to my app {str} {str1}");
+        Console.ReadLine();
+    }
+}
