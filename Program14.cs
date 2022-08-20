6edfa4ea918cce3a09634bedd7175f4f582ca43d
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-21 01:16:13 +0530

    Add files via upload

---
+++ Program14.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+
+namespace Demo
+{
+
+    class projec14
+    {
+
+        static void Main(string[] args)
+        {
+
+            double fahrenheit;
+
+            double celsius = 36;
+            Console.WriteLine("Celsius: " + celsius);
+
+            fahrenheit = (celsius * 9) / 5 + 32;
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+
+            Console.ReadLine();
+        }
+    }
+}
