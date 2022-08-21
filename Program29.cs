fddcec1db35dc485811511b0259be72a438b2fea
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-21 23:10:51 +0530

    Add files via upload

---
+++ Program29.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+
+namespace project29
+{
+    internal class problem29
+    {
+        static void Main()
+        {
+            int i;
+            Console.Write("Enter an Number: ");
+            i = Convert.ToInt32(Console.ReadLine());
+            if (i == 0)
+                Console.WriteLine("The given is zero");
+            else if (i > 100)
+                Console.WriteLine("the given nuber is too large");
+            else if (i % 10 == 0)
+                Console.WriteLine("Given number is multiple of 10");
+            else if (i % 2 == 0)
+                Console.WriteLine("The given number is even");
+            else if (i % 2 != 0)
+                Console.WriteLine("Given number is odd");
+
+            Console.ReadLine();
+        }
+    }
+}
