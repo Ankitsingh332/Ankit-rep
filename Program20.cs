b6dcc8ea3daa7affb78536407ca4c81c76717ed5
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-21 17:57:00 +0530

    Add files via upload

---
+++ Program20.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+
+namespace ex16
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string str1;
+            string str2;
+
+            
+            Console.Write("Enter a string: ");
+            str1 = Console.ReadLine();
+            Console.Write("Enter another string: ");
+            str2 = Console.ReadLine();
+
+             
+            if (str1 == str2)
+                Console.WriteLine("\"{0}\" and \"{1}\" are equal", str1, str2);
+            else
+                Console.WriteLine("\"{0}\" and \"{1}\" are not equal", str1, str2);
+
+           
+            if ((str1 == str2) == true)
+                Console.WriteLine("\"{0}\" and \"{1}\" are equal", str1, str2);
+            else
+                Console.WriteLine("\"{0}\" and \"{1}\" are not equal", str1, str2);
+
+            
+            Console.WriteLine("By ignoring case...");
+            if (str1.ToUpper() == str2.ToUpper())
+                Console.WriteLine("\"{0}\" and \"{1}\" are equal", str1, str2);
+            else
+                Console.WriteLine("\"{0}\" and \"{1}\" are not equal", str1, str2);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
