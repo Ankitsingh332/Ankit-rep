9f5af75abf2911c310e87bf13b577421e04f50c5
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-21 23:31:43 +0530

    Add files via upload

---
+++ Program30.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+
+namespace project30
+
+{
+    internal class Problem30
+    {
+        static void Main()
+        {
+            int i;
+            Console.WriteLine("Enter a number : ");
+            i = Convert.ToInt32(Console.ReadLine());
+            switch (i % 2)
+            {
+                case 0:
+                    Console.WriteLine("the number is even");
+                    break;
+                case 1:
+                    Console.WriteLine("The number is odd");
+                    break;
+                default:
+                    Console.WriteLine("the number is zero");
+                    break;
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
