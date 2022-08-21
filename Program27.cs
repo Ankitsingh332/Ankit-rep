48310fcf1f61b4f11770957c9b1b30e36c44c4e3
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-21 22:59:32 +0530

    Add files via upload

---
+++ Program27.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+
+namespace project27
+{
+    internal class problem11
+    {
+        static void Main()
+        {
+            int i, sum = 0;
+            for (i = 101; i < 200; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
