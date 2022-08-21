1bf65bd8411d6790d9cdd46dbbcbe8baaeb9e09c
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-21 23:32:47 +0530

    Add files via upload

---
+++ Program31.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace  project31
+{
+    internal class Problem31
+    {
+        static void Main()
+        {
+            int i = 0, j = 25;
+            while (i <= j)
+            {
+                Console.WriteLine($"i = {i} and j = {j}");
+                i++;
+                j--;
+            }
+            Console.WriteLine("Crossed Over");
+            Console.ReadLine();
+
+        }
+    }
+}
