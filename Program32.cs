ce5e60ef901660228776ccbd2ccf1cb2051928a8
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-21 23:33:23 +0530

    Add files via upload

---
+++ Program32.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+
+namespace project32
+{
+    internal class Problem32
+    {
+        static void Main()
+        {
+            int player = 100;
+            string str;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Enter HIT or MISS : ");
+                str = Console.ReadLine();
+                if (str == "HIT")
+                    player += 10;
+                else if (str == "MISS")
+                    player -= 20;
+
+            }
+            Console.WriteLine($"The player score after 4 shoot: {player}");
+        }
+    }
+}
