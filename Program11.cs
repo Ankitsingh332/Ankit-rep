90e476244cf96181ed5b6f21c05664fa7c6e2b1c
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-22 23:40:19 +0530

    Add files via upload

---
+++ Program11.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22th_hands_on
+{
+    internal class Problem11
+    {
+        static void Main(string[] args)
+        {
+            SortedList s1 = new SortedList();
+            s1.Add(3, "Ankit");
+            s1.Add(2, "Amit");
+            s1.Add(1, "pushkar");
+            s1.Add(4, "vini");
+            foreach (DictionaryEntry item in s1)
+            {
+                Console.WriteLine(item.Key + "  " + item.Value);
+            }
+        }
+    }
+}
