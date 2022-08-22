a630a06f09b821414f8120a66287d3e9bf85a6ba
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-22 23:39:33 +0530

    Add files via upload

---
+++ Program9.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22th_hands_on
+{
+    internal class Problem9
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            al.Add(7.5f);
+
+            al.Add(2.3f);
+            al.Add(1.5f);
+            al.Add(3.4f);
+            al.Add(4.4f);
+            al.Add(4.6f);
+            al.Add(1.2f);
+            Console.WriteLine("IN Descending order: ");
+            al.Sort();
+            foreach (float i in al)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("IN asscending order: ");
+            al.Sort();
+            foreach (float i in al)
+            {
+                Console.WriteLine(i);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
