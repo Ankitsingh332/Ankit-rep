996af7177762d85457957fade17787b24ffa04cb
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-22 23:39:54 +0530

    Add files via upload

---
+++ Program10.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+
+namespace Ex10
+{
+    internal class problem10
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            dic.Add(1, "Ankit");
+            dic.Add(2, "Amit");
+            dic.Add(3, "Pushkar");
+            dic.Add(4, "vini");
+
+            foreach (KeyValuePair<int, string> item in dic)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
