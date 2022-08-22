51aa016dc77b70d1f65bf96565851c33344623bd
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-22 23:41:25 +0530

    Add files via upload

---
+++ Program13.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+
+namespace Ex13
+{
+    internal class Problem13
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 3, 5, 200, 22, 2244, 213, 111 };
+            Console.WriteLine("number greater then 100: ");
+            foreach (int i in arr)
+            {
+                if (i > 100)
+                {
+
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
