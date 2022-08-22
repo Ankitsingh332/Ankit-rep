7074390c34ee6bd5f238dffbd59917bb65375c6f
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-22 23:38:42 +0530

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project7
+{
+    internal class problem7
+    {
+        static void Main(string[] args)
+        {
+
+            Queue s = new Queue(2);
+            s.Enqueue(1);
+            s.Enqueue(2);
+            s.Enqueue(3);
+            s.Enqueue("Ankit");
+            s.Enqueue("Singh");
+            Console.WriteLine("Peek value");
+            object t = s.Peek();
+            Console.WriteLine(t);
+            int cnt = s.Count;
+            Console.WriteLine("Bofore deqeueing the size of stack");
+            Console.WriteLine(cnt);
+            object i = s.Dequeue();
+            Console.WriteLine("deqeued value:");
+            Console.WriteLine(i);
+            int cnt2 = s.Count;
+            Console.WriteLine("After perfome deqeueing the size of stack");
+            Console.WriteLine(cnt2);
+        }
+    }
+}
+
