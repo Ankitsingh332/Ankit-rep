7304d5a05b4b3ce49f20bd5abc7c8fcaac72c495
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-22 23:39:07 +0530

    Add files via upload

---
+++ Program8.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22th_hands_on
+{
+    internal class Problem8
+    {
+        static void Main(string[] args)
+        {
+            Hashtable h = new Hashtable();
+            h.Add(1, "Bihar");
+            h.Add(2, "UttarPradesh");
+            h.Add(3, "Jharkhand");
+            h.Add(4, "Delhi");
+            h.Add(5, "Punjab");
+            IDictionaryEnumerator ie = h.GetEnumerator();
+            while (ie.MoveNext())
+            {
+                {
+                    Console.WriteLine(ie.Key + "  " + ie.Value);
+                }
+                Console.WriteLine("Using for each");
+                foreach (DictionaryEntry item in h)
+                {
+                    Console.WriteLine(item.Key + "  " + item.Value);
+
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
