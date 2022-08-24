ca2e283ec5582c99600fd171cfb90f56f4506e1a
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-25 00:16:03 +0530

    Add files via upload

---
+++ program3.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReusableCodeLibrary
+{
+    public class program3
+    {
+        public static void Index(string str, char ch)
+        {
+            int i = str.IndexOf(ch);
+            Console.WriteLine($"Index of {ch} in given string {str} is : {i}");
+            Console.WriteLine();
+
+        }
+    }
