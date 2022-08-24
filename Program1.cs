0b70841747b888e5ecabdf0e420debcc82617ae0
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-24 12:32:38 +0530

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23Aug
+{
+    class Program
+    {
+        static void add(int i, int j)
+        {
+            int sum = i + j;
+            Console.WriteLine("The sum of two integer given by the user: " + sum);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int j = Convert.ToInt32(Console.ReadLine());
+            add(i, j);
+            Console.ReadLine();
+
+        }
+    }
+}
+
