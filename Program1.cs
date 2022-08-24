4d00b785ebc239c97466b71f4d70491a85a9b875
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-25 00:14:54 +0530

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReusableCodeLibrary
+{
+    public class program1
+    {
+        public static void Compare(string str1, string str2)
+        {
+            if (str1.Equals(str2))
+                Console.WriteLine("Both strings are equal.");
+            else
+                Console.WriteLine("Strings are not equal");
+
+            Console.WriteLine();
+        }
+    }
+}
