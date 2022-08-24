983aeaf4c8c57307b42b787e7f421b7ea7ec1fb7
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-25 00:17:17 +0530

    Add files via upload

---
+++ program6.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReusableCodeLibrary
+{
+    public class program6
+    {
+        public static void FirstFile()
+        {
+            string path = @"D:\Mphasis 24th\exercise\MyApp\FirstFile.txt";
+            File.Create(path);
+        }
+    }
+}
