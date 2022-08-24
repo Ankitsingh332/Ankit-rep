280188d60527418307f2602e55fdab0269a452af
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-25 00:16:52 +0530

    Add files via upload

---
+++ program5.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReusableCodeLibrary
+{
+    public class program5
+    {
+        public static void Case(string str)
+        {
+            Console.WriteLine(str.ToUpper());
+            Console.ReadLine();
+        }
+    }
+}
