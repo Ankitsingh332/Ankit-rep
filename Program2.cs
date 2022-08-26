0f6e537876c45154f680730212e4b65acbad82db
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-26 14:32:17 +0530

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    internal class Problem2
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\mphasis\25th August\program2";
+            Console.WriteLine("Enter the file name");
+            string fname = Console.ReadLine();
+            fname = string.Concat(path, fname);
+
+            // Statemnet a
+            //File.Create(fname);
+            //File.WriteAllText(fname, "Hello, I am learning C# & .net");
+            //Console.WriteLine($"{fname} File Created...");
+
+            //Statement b and c
+            //fname = string.Concat(fname,"\\");
+            //File.Copy(fname, "copied.txt");
+            //File.Move(fname, "movedfile.txt");
+
+            // Checking file existance
+            bool fileExists = File.Exists(fname);
+            Console.WriteLine("File Exists=  " + fileExists);
+
+            //Delete file
+            File.Delete(fname);
+
+
+
+
+            // Directory of the file-- problem statement g
+            FileInfo fi = new FileInfo(fname);
+            string str = fi.DirectoryName;
+            Console.WriteLine($"{str} is directory name...");
+
+            // problem statement f -- creation time of the file..
+            DateTime dt = new DateTime();
+            dt = fi.CreationTime;
+            Console.WriteLine($"Creation time of the file : {dt}");
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
