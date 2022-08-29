eebacf47e5cef49f7316e0382aed06937c06036c
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-08-29 14:44:03 +0530

    Update ArrayStudentObject.cs

--- Console_file/ArrayStudentObject.cs
+++ Console_file/ArrayStudentObject.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HRLibrary;
-namespace _26th_Hands_on
+namespace 26th_Handson
 {
     internal class ArrayStudentObject
     {
