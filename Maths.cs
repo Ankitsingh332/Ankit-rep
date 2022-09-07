b0c9a386cc62b86ea4990039b111ca652b43d99c
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-09-07 23:54:00 +0530

    Update Maths.cs

--- Maths.cs
+++ Maths.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DelegatesLibrary
+namespace 7th_sep
 {
     public delegate void MathsOpr(int i, int j);
     public class Maths
