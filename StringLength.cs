067c5615a8061cb9cba542b204e41d775975095a
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-09-08 00:06:01 +0530

    Add files via upload

---
+++ StringLength.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7th_sep
+{
+    public delegate int StringLenghtDelegate(string s);
+    public class StringLength
+    {
+        public int len(string s)
+        {
+            int i = s.Length;
+            return i;
+        }
+    }
+}
