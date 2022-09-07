05c0c85828976318165c315e82d4ae6f171adcfc
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-09-08 00:04:56 +0530

    Add files via upload

---
+++ Getting.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  _7th_sep
+{
+    public delegate string GreetDelegate(string Name);
+    public class Geeting
+    {
+
+        public string GreetUser(string Name)
+        {
+            string greetmsg = "Hello " + Name + " , Welcome in my world!";
+            return greetmsg;
+        }
+    }
+}
+
